23c1561daba5e21bdb68bd9dbc30d126c1dc0a4d
Tonmoy-softzino <pramaniktonmoy2@gmail.com>
2023-05-01 16:04:57 +0600

    Project upload on github

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import SingUp from './components/SingUp.vue';
+</script>
+
+<template>
+  <div>
+    <SingUp/>
+    
+  </div>
+
+</template>
+
+<style scoped>
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/SingUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!-- <p>Count: {{ count }}</p>
+    <button @click="incrementCount">Increment</button> -->
+
+    <input :value="text" @input="formFill" placeholder="type here"/>
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    // const count = ref(0);
+    const text = ref('');
+    const formFill = (event) => {
+      text.value = event.target.value;
+    };
+
+    // const incrementCount = () => {
+    //   count.value++;
+    // };
+
+    return {
+      // count,
+      text,
+      formFill,
+      // incrementCount,
+    }
+  },
+};
+</script>
+
+<style>
+.text-green {
+  color: green;
+}
+.text-red {
+  color: red;
+}
+</style>
